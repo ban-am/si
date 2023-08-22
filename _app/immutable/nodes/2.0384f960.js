13cdf75ef977d2fb11ccbd853c7cdbdef45f5c99
Matej Bandik <stbandk@gmail.com>
2023-08-22 22:26:53 +0200

    gh-pages

---
+++ _app/immutable/nodes/2.0384f960.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/2.86f15a0f.js";export{m as component};
--- src/store.ts
+++
@@ -1,6 +0,0 @@
-import type { DataModel } from "$lib";
-import { writable } from "svelte/store";
-
-export const dataHash: string = "U2FsdGVkX1/lsEyqjjipjZ2aJQM2jBJygOD+H646qfPr4jH4i2L3NApX7OXLnJe5ud+n+TV3HD7W4tQvZsXxN1Hte1bFn2bVxo8RE4iMpWqL0InGEZtjJxQOAMtDF6MyhA7w64XRPoBXHl07sPzO3n949Z6kFAgrGy4YViI5UM9no1acUYVvELN/Lr75A0JEOsQztDmv/WPO6LPWYXdi3Q==";
-
-export const dataModel = writable<DataModel | undefined>();
